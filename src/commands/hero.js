aa65b06b35438fb4bada942046d79efa741faa5c
Naist <alves.vinicius9@hotmail.com>
2022-04-28 16:20:27 -0300

    Build embed message template
    Finished build embed message template + Elesis' data.

--- src/commands/hero.js
+++ src/commands/hero.js
@@ -11,34 +11,92 @@ module.exports = {
 		.setDescription("Replies with SR heroes data.")
 		.addStringOption(option =>
 			option.setName("hero-name")
-				.setDescription("The hero to be retrieved.")
-				.setRequired(true)),
+				.setDescription("The hero's base-specifics to be retrieved.")
+				.setRequired(true))
+		.addStringOption(option =>
+			option.setName("info")
+				.setDescription("The hero's specifics to be retrieved.")
+				.setRequired(false)),
+
 	async execute(interaction) {
 		const rawHerodata = fs.readFileSync('./src/hero-library/library.json');
-		const heroData = JSON.parse(rawHerodata);
-		const chosenHero = interaction.options.getString("hero-name");
-		// console.log("chosen hero: " + chosenHero);
+		const heroDataJSON = JSON.parse(rawHerodata);
+		const chosenHero = heroDataJSON[interaction.options.getString("hero-name")];
+		const heroWhichInfo = interaction.options.getString("info");
+		
+		if (chosenHero === undefined) return;
 
-		const heroSpecifics = new MessageEmbed()
-			// .setColor(heroData.color)
-			// .setTitle(heroData.title)
-			.setColor('#0099ff')
-			.setTitle('Some title')
-			.setURL('https://discord.js.org/')
-			.setAuthor({ name: interaction.user.tag, iconURL: interaction.user.displayAvatarURL() })
-			.setDescription('Some description here')
-			.setThumbnail('https://i.imgur.com/AfFp7pu.png')
-			.addFields(
-				{ name: 'Regular field title', value: 'Some value here' },
-				{ name: '\u200B', value: '\u200B' },
-				{ name: 'Inline field title', value: 'Some value here', inline: true },
-				{ name: 'Inline field title', value: 'Some value here', inline: true },
-			)
-			.addField('Inline field title', 'Some value here', true)
-			.setImage('https://i.imgur.com/AfFp7pu.png')
+
+		if (chosenHero["enabled"]) {
+
+			// Base template for hero's Embed Messages info
+			const heroEmbed = new MessageEmbed()
+			.setColor(chosenHero["color"])
+			.setTitle(chosenHero["title"])
+			.setDescription(chosenHero["hero-description"])
+			.setThumbnail(chosenHero["hero-thumbnail"])
+			.setImage(chosenHero["hero-banner"])
 			.setTimestamp()
-			.setFooter({ text: 'Some footer text here', iconURL: 'https://i.imgur.com/AfFp7pu.png' });
+			.setFooter({ text: 'Credits: Steam Mariestad#7547 \nLast updated on ' + chosenHero["last-update"], iconURL: interaction.user.displayAvatarURL() });
 
-		await interaction.reply({ embeds: [heroSpecifics] });
+
+
+			// Choose whether you ask for base or LB's hero info
+			if (heroWhichInfo == "base" || heroWhichInfo == "" || heroWhichInfo == undefined) {
+				heroEmbed.addFields(
+					{ name: chosenHero["passive-name"], value: chosenHero["passive-description"] },
+					{ name: '\u200B', value: '\u200B' },
+					{ name: chosenHero["s1-name"], value: chosenHero["s1-description"], inline: true },
+					{ name: chosenHero["s2-name"], value: chosenHero["s2-description"], inline: true },
+					{ name: '\u200B', value: '\u200B' },
+				);
+				heroEmbed.addField(chosenHero["ss-name"], chosenHero["ss-description"]);
+
+
+				await interaction.reply({ embeds: [heroEmbed] });
+			}
+			else if (heroWhichInfo === "lb") {
+				// console.log("lb:" + chosenHero["lb"]["passive-name"]);
+				heroEmbed.addFields(
+					{ name: chosenHero["lb"]["passive-name"], value: chosenHero["lb"]["passive-description"] },
+					{ name: '\u200B', value: '\u200B' },
+					{ name: chosenHero["lb"]["s1-name"], value: chosenHero["lb"]["s1-description"], inline: true },
+					{ name: chosenHero["lb"]["s2-name"], value: chosenHero["lb"]["s2-description"], inline: true },
+					{ name: '\u200B', value: '\u200B' },
+				);
+
+				await interaction.reply({ embeds: [heroEmbed] });
+			}
+			else if (heroWhichInfo === "build") {
+
+				heroEmbed.addFields(
+					{ name: 'Recommended Set(s): ', value: chosenHero["build"]["sets"] },
+					{ name: '\u200B', value: '\u200B' },
+					{ name: 'Recommended Traits: ', value: chosenHero["build"]["traits"] },
+					{ name: '\u200B', value: '\u200B' },
+					{ name: 'Recommended LB Skills: ', value: chosenHero["build"]["lb-skills"] },
+					{ name: '\u200B', value: '\u200B' },
+					{ name: 'Recommended Accessories: ', value: chosenHero["build"]["accessories"] },
+					{ name: '\u200B', value: '\u200B' },
+					{ name: 'PvE priority: ', value: chosenHero["build"]["priority"]["PvE"], inline: true },
+					{ name: 'PvP priority: ', value: chosenHero["build"]["priority"]["PvP"], inline: true },
+					{ name: 'WB priority: ', value: chosenHero["build"]["priority"]["WB"], inline: true },
+					{ name: '\u200B', value: '\u200B' },
+					{ name: 'Chaser Build', value: 'RANK :zero::five: | ' + chosenHero["build"]["chaser-traits"]["cl5"] +
+													'\nRANK :one::zero: | ' + chosenHero["build"]["chaser-traits"]["cl10"] +
+													'\nRANK :one::five: | ' + chosenHero["build"]["chaser-traits"]["cl15"] +
+													'\nRANK :two::zero: | ' + chosenHero["build"]["chaser-traits"]["cl20"] },
+					// { name: 'asd', value: 'asd' },
+					// { name: '\u200B', value: chosenHero["build"]["chaser-traits"]["cl15"] },
+					// { name: '\u200B', value: chosenHero["build"]["chaser-traits"]["cl20"] },
+				);
+
+				await interaction.reply({ embeds: [heroEmbed] });
+			}
+			else {
+				await interaction.reply({ content: `${chosenHero["hero-short-name"]} specifics are temporarily unavailable. Try again later.`, ephemeral: true });
+			}
+		}
+		
 	},
 };
