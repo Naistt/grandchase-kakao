62e5be2495c84421bf84592fb09c12f7c6e145a9
Naist <alves.vinicius9@hotmail.com>
2023-03-18 15:30:10 -0300

    bov calc + library update
    bov calculator command + 3 hero updates.

---
+++ src/commands/bov.js
@@ -0,0 +1,34 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('bcalc')
+		.setDescription('Calculates how many BoVs you need to reach a specific level.')
+        .addIntegerOption(option =>
+			option.setName("current-level")
+				.setDescription("The hero's type current level.")
+				.setRequired(true))
+		.addIntegerOption(option =>
+			option.setName("target-level")
+				.setDescription("The hero's type aimed level.")
+				.setRequired(true)),
+	async execute(interaction) {
+		let iBovAmount = 0;
+		const iCurrentLevel = interaction.options.getInteger("current-level");
+		const iAimedLevel = interaction.options.getInteger("target-level");
+
+		if (iAimedLevel <= 480) {
+			if (iCurrentLevel > 0 && iCurrentLevel < iAimedLevel) {
+				iBovAmount = (iAimedLevel * iAimedLevel + iAimedLevel) - (iCurrentLevel * iCurrentLevel + iCurrentLevel);
+				await interaction.reply(`Amount of Blessings of Valor required from level ${iCurrentLevel} to level ${iAimedLevel} is ${iBovAmount}.`);
+			}
+			else {
+				await interaction.reply(`The current level must be above zero (0) and lesser than the target level.`);
+			}
+		}
+		else {
+			await interaction.reply(`The current max level is 480.`);
+		}
+		
+	},
+};
