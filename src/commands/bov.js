aeb4b6a36ea64ed7c68eb314c4af8029fc4fc6a7
Naist <alves.vinicius9@hotmail.com>
2024-01-31 11:45:59 -0300

    Desabilitando 'gvg' e 'build'
    Desabilitando 'gvg' e 'build' e adicionando novos níveis no 'bov'

--- src/commands/bov.js
+++ src/commands/bov.js
@@ -3,31 +3,31 @@ const { SlashCommandBuilder } = require('discord.js');
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('bcalc')
-		.setDescription('Calculates how many BoVs you need to reach a specific level.')
+		.setDescription('Calcula quantas BoVs você precisa para chegar em determinado nível.')
         .addIntegerOption(option =>
-			option.setName("current-level")
-				.setDescription("The hero's type current level.")
+			option.setName("nivel-atual")
+				.setDescription("Nível atual da maestria.")
 				.setRequired(true))
 		.addIntegerOption(option =>
-			option.setName("target-level")
-				.setDescription("The hero's type aimed level.")
+			option.setName("nivel-alvo")
+				.setDescription("Nível alvo da maestria.")
 				.setRequired(true)),
 	async execute(interaction) {
 		let iBovAmount = 0;
-		const iCurrentLevel = interaction.options.getInteger("current-level");
-		const iAimedLevel = interaction.options.getInteger("target-level");
+		const iCurrentLevel = interaction.options.getInteger("nivel-atual");
+		const iAimedLevel = interaction.options.getInteger("nivel-alvo");
 
-		if (iAimedLevel <= 600) {
+		if (iAimedLevel <= 640) {
 			if (iCurrentLevel > 0 && iCurrentLevel < iAimedLevel) {
 				iBovAmount = (iAimedLevel * iAimedLevel + iAimedLevel) - (iCurrentLevel * iCurrentLevel + iCurrentLevel);
-				await interaction.reply(`Amount of Blessings of Valor required from level ${iCurrentLevel} to level ${iAimedLevel} is ${iBovAmount}.`);
+				await interaction.reply(`Quantidade de BoVs requeridas do nível ${iCurrentLevel} até ${iAimedLevel} é de ${iBovAmount}.`);
 			}
 			else {
-				await interaction.reply(`The current level must be above zero (0) and lesser than the target level.`);
+				await interaction.reply(`O nível atual deve ser acima de zero e menor que o nível alvo.`);
 			}
 		}
 		else {
-			await interaction.reply(`The current max level is 600.`);
+			await interaction.reply(`O nível máximo atual é 640.`);
 		}
 		
 	},
--- src/commands/build.js
+++ src/commands/build.js
@@ -73,6 +73,12 @@ module.exports = {
 		
 	},			
 	async execute(interaction) {
+		// eslint-disable-next-line no-constant-condition
+		if (true) {
+			await interaction.reply({ content: 'Desabilitado temporariamente.', ephemeral: true });
+			return;
+		}
+		
         await interaction.deferReply();
 
 		const rawHerodata = fs.readFileSync('./src/hero-library/library.json');
@@ -96,8 +102,32 @@ module.exports = {
 		const heroBanner = inputHero["build"]["banner_id"];
 		
 		try {
-			const background = await Canvas.loadImage(`./src/img/Oblivious/chars/${heroBanner}.png`);
+			const background = await Canvas.loadImage(`./src/img/new/Greatkounatexplosion.png`);
+            const blackOpacity = await Canvas.loadImage(`./src/img/new/rectangle_50_opacity.png`);
 			context.drawImage(background, 0, 0, canvas.width, canvas.height);
+            context.drawImage(blackOpacity, 0, 0, canvas.width, canvas.height);
+
+            const char = await Canvas.loadImage(`./src/img/new/elesis.png`);
+            const shadow = await Canvas.loadImage(`./src/img/new/elesis_shadow.png`);
+            context.drawImage(shadow, -200, 0, canvas.width, canvas.height);
+            context.drawImage(char, -200, 0, canvas.width, canvas.height);
+
+
+            // Select the font size and type from one of the natively available fonts
+            context.font = '40px Gill Sans sans-serif';
+
+            // Select the style that will be used to fill the text in
+            context.fillStyle = '#ffffff';
+
+            // Actually fill the text with a solid color
+            context.fillText("SET", canvas.width / 2.5, canvas.height / 6);
+			context.fillText("ENCANT. MISTICO", canvas.width / 2.5, canvas.height / 4);
+			context.fillText("ESPECIALIZACAO", canvas.width / 2.5, canvas.height / 3);
+			context.fillText("HAB. APRIMORADA", canvas.width / 2.5, canvas.height / 2.4);
+			context.fillText("ESP. TRAN.", canvas.width / 2.5, canvas.height / 2);
+			context.fillText("ACESSORIOS", canvas.width / 2.5, canvas.height / 1.7);
+			context.fillText("CHASER SKILL", canvas.width / 2.5, canvas.height / 1.5);
+            
 		}
 		catch (error) {
 			console.log(`Plano de fundo não foi carregado pelo Canva. Linha 99.`);
@@ -108,313 +138,7 @@ module.exports = {
 		
 
 
-		try {
-			const setColor = await Canvas.loadImage(`./src/img/sets/set_${inputHero["build"]["sets"]}.png`);
 		
-			// PvE set
-			context.drawImage(setColor, 635, 60, 75, 75);
-			// PvP set
-			context.drawImage(setColor, 835, 60, 75, 75);
-		}
-		catch (error) {
-			console.log(`Cor do equipamento não foi carregado pelo Canva. Linha 112.`);
-			console.log(error);
-			await interaction.editReply("Houve um erro ao carregar as informações do personagem. Entre em contato com <@197514134349283328>.");
-			return;
-		}
-		
-
-		// This uses the canvas dimensions to stretch the image onto the entire canvas
-		
-
-
-		
-
-		// Enchantment
-		context.font = '23px sans-serif';
-		context.fillStyle = '#ffffff';
-		context.fillText(`${inputHero["build"]["enchantment"]["white"]}`, canvas.width / 1.58, canvas.height / 4.1);
-		context.fillText(`${inputHero["build"]["enchantment"]["red"]}`, canvas.width / 1.31, canvas.height / 4.1);
-		context.fillText(`${inputHero["build"]["enchantment"]["blue"]}`, canvas.width / 1.12, canvas.height / 4.1);
-
-
-
-
-
-		// Level Traits Block - 80 em 80 ou 120 em 120
-		let arr = inputHero["build"]["level-traits"];
-		let dx = 835;
-		// console.log("ARR LENGTH: " + Object.keys(arr).length);
-		for (let i = 1; i < Object.keys(arr).length + 1; i++) {
-			// console.log("11");
-			// console.log(arr);
-			
-			try {
-				const lvlTrait = await Canvas.loadImage(`./src/img/level traits/${arr[i]}.png`);
-			
-				context.drawImage(lvlTrait, dx, 280, 75, 75);
-
-				dx += 120;
-			}
-			catch (error) {
-				console.log(`Erro ao carregar especializações de nível. Linha 153.`);
-				console.log(error);
-				await interaction.editReply("Houve um erro ao carregar as informações do personagem. Entre em contato com <@197514134349283328>.");
-				return;
-			}
-			
-			
-			
-			
-			// console.log(arr[i]);
-			
-		}
-
-		// let lvlTrait = await Canvas.loadImage(`./src/img/level traits/${}.png`);
-		// context.drawImage(lvlTrait, 835, 280, 75, 75);
-		// context.drawImage(lvlTrait, 955, 280, 75, 75);
-		// context.drawImage(lvlTrait, 1075, 280, 75, 75);
-		// context.drawImage(lvlTrait, 1195, 280, 75, 75);
-		// // context.fillText("CHANCE DE CRÍTICO, AUMENTO DE DANO AO IGNORAR DEFESA,", canvas.width / 1.9, canvas.height / 3.1);
-
-
-
-
-
-		// HABILIDADE APRIMORADA PVE
-		arr = inputHero["build"]["lb-skills"];
-		const id = inputHero["build"]["banner_id"];
-		dx = 1000;
-
-		for (let i = 1; i < Object.keys(arr).length + 1; i++) {
-			// console.log("ARR:" + arr[i]);
-			// console.log("22");
-			// console.log(arr);
-			
-			try {
-				const lbSkill = await Canvas.loadImage(`./src/img/lb skills/${id}_${arr[i]}.png`);
-			
-				context.drawImage(lbSkill, dx, 395, 75, 75);
-
-				dx += 100;
-			}
-			catch (error) {
-				console.log(`Erro ao carregar aprimoramento de habilidade (LB) PvE. Linha 195.`);
-				console.log(error);
-				await interaction.editReply("Houve um erro ao carregar as informações do personagem. Entre em contato com <@197514134349283328>.");
-				return;
-			}
-		}
-
-
-		// HABILIDADE APRIMORADA PVP
-		arr = inputHero["build"]["lb-skills-pvp"];
-		dx = 1290;
-
-		for (let i = 1; i < Object.keys(arr).length + 1; i++) {
-			try {
-				const lbSkill = await Canvas.loadImage(`./src/img/lb skills/${id}_${arr[i]}.png`);
-			
-				context.drawImage(lbSkill, dx, 395, 75, 75);
-				// console.log("33");
-				// console.log(arr);
-				
-				dx += 100;
-			}
-			catch (error) {
-				console.log(`Erro ao carregar aprimoramento de habilidade (LB) PvP. Linha 216.`);
-				console.log(error);
-				await interaction.editReply("Houve um erro ao carregar as informações do personagem. Entre em contato com <@197514134349283328>.");
-				return;
-			}
-		}
-
-
-
-		// TRANSCENDANT TRAITS BLOCK
-		arr = inputHero["build"]["transcend-traits"];
-		dx = 1075;
-		// console.log("ARR LENGTH: " + Object.keys(arr).length);
-		for (let i = 1; i < Object.keys(arr).length + 1; i++) {
-			try {
-				const tTrait = await Canvas.loadImage(`./src/img/transcend traits/${arr[i]}.png`);
-			
-				context.drawImage(tTrait, dx, 510, 75, 75);
-				// console.log("44");
-				// console.log(arr);
-
-				// console.log(arr[i]);
-				dx += 100;
-			}
-			catch (error) {
-				console.log(`Erro ao carregar especializações transcendentes. Linha 240.`);
-				console.log(error);
-				await interaction.editReply("Houve um erro ao carregar as informações do personagem. Entre em contato com <@197514134349283328>.");
-				return;
-			}
-		}
-		// const ttTrait = await Canvas.loadImage('./src/img/transcend traits/tt_unlim_power.png');
-		// context.drawImage(ttTrait, 1075, 510, 75, 75);
-		// context.drawImage(ttTrait, 1175, 510, 75, 75);
-		// context.drawImage(ttTrait, 1275, 510, 75, 75);
-		// context.drawImage(ttTrait, 1375, 510, 75, 75);
-
-
-
-		// ACCESSORIES PvE BLOCK
-		arr = inputHero["build"]["accessories"];
-		dx = 765;
-		let strCorAcc = inputHero["build"]["acc-color"];
-		// console.log(corAcc);
-		for (let i = 1; i < Object.keys(arr).length + 1; i++) {
-			try {
-				let acc = await Canvas.loadImage(`./src/img/accessories/${arr[i]}.png`);
-				let corAcc = await Canvas.loadImage(`./src/img/accessories/${strCorAcc}.png`);
-
-				context.drawImage(corAcc, dx, 620, 75, 75);
-				context.drawImage(acc, dx, 620, 75, 75);
-
-
-				// console.log("55");
-				// console.log(arr);
-
-				// console.log(arr[i]);
-				dx += 80;
-			}
-			catch (error) {
-				console.log(`Erro ao carregar acessórios PvE. Linha 271 e/ou 272.`);
-				console.log(error);
-				await interaction.editReply("Houve um erro ao carregar as informações do personagem. Entre em contato com <@197514134349283328>.");
-				return;
-			}
-		}
-		// let acc = await Canvas.loadImage('./src/img/accessories/lava.png');
-		// context.drawImage(acc, 765, 620, 75, 75);
-		// context.drawImage(acc, 845, 620, 75, 75);
-		// context.drawImage(acc, 925, 620, 75, 75);
-
-
-
-		// RELIC PvE BLOCK
-		let relic = inputHero["build"]["relic"];
-		let relicImg;
-
-		try {
-			relicImg = await Canvas.loadImage(`./src/img/relics/${relic}.png`);
-
-			context.drawImage(relicImg, 995, 620, 75, 75);
-
-
-			// console.log("55");
-			// console.log(arr);
-
-			// console.log(arr[i]);
-			dx += 80;
-		}
-		catch (error) {
-			console.log(`Erro ao carregar relíquia PvE. Linha 303.`);
-			console.log(error);
-			await interaction.editReply("Houve um erro ao carregar as informações do personagem. Entre em contato com <@197514134349283328>.");
-			return;
-		}
-		
-		
-		
-
-
-
-
-
-		// ACCESSORIES PvP
-		arr = inputHero["build"]["accessories-pvp"];
-		dx = 1175;
-		strCorAcc = inputHero["build"]["acc-pvp-color"];
-		// console.log("ARR LENGTH: " + Object.keys(arr).length);
-		for (let i = 1; i < Object.keys(arr).length + 1; i++) {
-			try {
-				let acc = await Canvas.loadImage(`./src/img/accessories/${arr[i]}.png`);
-				let corAcc = await Canvas.loadImage(`./src/img/accessories/${strCorAcc}.png`);
-	
-				context.drawImage(corAcc, dx, 620, 75, 75);
-				context.drawImage(acc, dx, 620, 75, 75);
-				
-				// console.log("66");
-				// console.log(arr);
-
-				// console.log(arr[i]);
-				dx += 80;
-			}
-			catch (error) {
-				console.log(`Erro ao carregar acessórios PvP. Linha 335 e/ou 336.`);
-				console.log(error);
-				await interaction.editReply("Houve um erro ao carregar as informações do personagem. Entre em contato com <@197514134349283328>.");
-				return;
-			}
-			
-			
-			
-		}
-		// acc = await Canvas.loadImage('./src/img/accessories/lava.png');
-		// context.drawImage(acc, 1175, 620, 75, 75);
-		// context.drawImage(acc, 1255, 620, 75, 75);
-		// context.drawImage(acc, 1335, 620, 75, 75);
-
-		// // RELIC PvP
-		relic = inputHero["build"]["relic-pvp"];
-
-		try {
-			relicImg = await Canvas.loadImage(`./src/img/relics/${relic}.png`);
-
-			context.drawImage(relicImg, 1405, 620, 75, 75);
-		}
-		catch (error) {
-			console.log(`Erro ao carregar relíquia PvP. Linha 366.`);
-			console.log(error);
-			await interaction.editReply("Houve um erro ao carregar as informações do personagem. Entre em contato com <@197514134349283328>.");
-			return;
-		}
-		
-		
-		
-
-
-
-		// // CHASER TRAITS
-		let cl = interaction.options.getString('cl');
-		if (cl === undefined || cl === null) {
-			cl = 20;
-		}
-		console.log(cl);
-		// console.log(cl);
-		arr = inputHero["build"]["chaser-traits"][`cl${cl}`];
-		console.log("ARR: " + arr);
-		dx = 765;
-		console.log("ARR LENGTH: " + Object.keys(arr).length);
-		for (let i = 1; i < Object.keys(arr).length + 1; i++) {
-			try {
-				const cs = await Canvas.loadImage(`./src/img/chaser traits/${arr[i]}.png`);
-				
-				context.drawImage(cs, dx, 740, 75, 75);
-
-				// console.log("77");
-				// console.log(arr);
-
-				// console.log(arr[i]);
-				dx += 80;
-			}
-			catch (error) {
-				console.log(`${interaction.options.getString('personagem').toLowerCase()} não tem chaser skill.`);
-				console.log(error);
-				await interaction.editReply("Houve um erro ao carregar as informações do personagem. Entre em contato com <@197514134349283328>.");
-				return;
-			}
-			
-		}
-		// acc = await Canvas.loadImage('./src/img/chaser traits/ct_Impeto.png');
-		// context.drawImage(acc, 845, 740, 75, 75);
-		// context.drawImage(acc, 925, 740, 75, 75);
-		// context.drawImage(acc, 1005, 740, 75, 75);
-		// context.drawImage(acc, 1085, 740, 75, 75);
 		
 
 
